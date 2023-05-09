710efbc894f4f23f6d7401e53e5657f2fc1d5001
yoone12 <125273670+yoone12@users.noreply.github.com>
2023-05-09 10:19:45 -0400

    Add rewrites for static HTML pages

--- teafusion-nxt/next.config.js
+++ teafusion-nxt/next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    async rewrites() {
+        return [
+        {
+            source: '/',
+            destination: '/html/index.html', 
+        },
+        {
+            source:'/teas',
+            destination: '/html/teas.html',
+        },
+        {
+            source:'/teaware',
+            destination: '/html/teaware.html',
+        },
+        {
+            source:'/teas-product',
+            destination: '/html/teas-product.html',
+        },
+        {
+            source:'/about',
+            destination: '/html/about.html',
+        },
+        {
+            source:'/profile',
+            destination: '/html/profile.html',
+        },
+        ];
+    },
+}
 
 module.exports = nextConfig
