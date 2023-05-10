c1214c93a4dc21a004e02f6a8780e58847ee0127
yoone12 <125273670+yoone12@users.noreply.github.com>
2023-05-10 07:25:40 -0400

    Create Navbar component and import into profile

---
+++ teafusion-nxt/app/navbar.js
@@ -0,0 +1,27 @@
+import "public/styles/navbar.css";
+
+/* Navbar component */
+export default function Navbar(){
+    return(
+        <>
+            <div className="nav-spacer"></div>
+            <div className="top-bar"></div>
+            <div className="container">
+                <div className="navbar">
+                    <img src="assets/images/home/TeaFusion-logo.png" alt="TeaFusion logo"/>
+                    <ul className="nav">
+                        <li><a href='/'>Home</a></li>
+                        <li><a href="about">About Us</a></li>
+                        <li><a href="teas">Tea</a></li>
+                        <li><a href="teaware">Teaware</a></li>
+                    </ul>
+                    <div class="nav-icons">
+                        <img src="assets/images/home/icon-search.png" alt="" />
+                        <img src="assets/images/home/icon-profile.png" alt="" />
+                        <img src="assets/images/home/icon-bag.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- teafusion-nxt/app/profile/page.js
+++ teafusion-nxt/app/profile/page.js
@@ -1,35 +1,10 @@
+import Navbar from "../navbar.js";
 
 
-/* Navbar component */
-export default function Profile(){
+export default function profile(){
     return(
         <>
-            <div className="nav-spacer"></div>
-            <div className="top-bar"></div>
-            <div className="container">
-                <div className="navbar">
-                    <img src="assets/images/home/TeaFusion-logo.png" alt="TeaFusion logo"/>
-                    <ul className="nav">
-                        <li><a href='/'>Home</a></li>
-                        <li><a href="about">About Us</a></li>
-                        <li><a href="teas">Tea</a></li>
-                        <li><a href="teaware">Teaware</a></li>
-                    </ul>
-                    <div class="nav-icons">
-                        <img src="assets/images/home/icon-search.png" alt="" />
-                        <img src="assets/images/home/icon-profile.png" alt="" />
-                        <img src="assets/images/home/icon-bag.png" alt="" />
-                    </div>
-                </div>
-            </div>
+            <Navbar />
         </>
     )
 }
-
-/* export default function profile(){
-    return(
-        <>
-
-        </>
-    )
-} */
