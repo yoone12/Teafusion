7581c821b279a33b592e75c9a5c737f3ad64d64c
yoone12 <125273670+yoone12@users.noreply.github.com>
2023-05-10 07:40:00 -0400

    Add footer to profile

--- teafusion-nxt/app/profile/page.js
+++ teafusion-nxt/app/profile/page.js
@@ -1,10 +1,12 @@
 import Navbar from "../navbar.js";
-
+import Footer from "../footer.js";
 
 export default function profile(){
     return(
         <>
             <Navbar />
+                
+            <Footer />
         </>
     )
 }
