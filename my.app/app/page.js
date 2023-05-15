9a38b02c879bf5aa16c3857448f1ad4cc9facdeb
xeniahuang21 <xeniahuang21@gmail.com>
2023-05-15 19:56:58 -0400

    Convert index to page.js

---
+++ my.app/app/page.js
@@ -0,0 +1,86 @@
+import Navbar from "./navbar.js";
+import Footer from "./footer.js";
+import "../public/styles/style.css";
+
+export default function Home() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Teafusion</title>
+      <div className="top-bar" />
+      <div className="container">
+        <Navbar />
+        {/* Landing content */}
+        <div className="landing">
+          <div className="landing-image">
+            <img src="../assets/images/home/home-full-1.png" alt="" />
+            <a href="/teas">
+              <button>Discover our teas</button>
+            </a>
+          </div>
+        </div>
+        {/* Menu images */}
+        <div className="menu">
+          <div className="menuitem">
+            <a href="/teas">
+              <img src="../assets/images/home/hover-teas.png" alt="" />
+            </a>
+          </div>
+          <div className="menuitem">
+            <a href="">
+              <img src="../assets/images/home/hover-spices.png" alt="" />
+            </a>
+          </div>
+          <div className="menuitem">
+            <a href="">
+              <img src="../assets/images/home/hover-wares.png" alt="" />
+            </a>
+          </div>
+        </div>
+        {/* Image, Parallax(?) */}
+        <img src="../assets/images/home/home-full-2.png" className="img-full-width" alt="" />
+        {/* About us block */}
+        <div className="block">
+          <div className="left">
+            <img src="../assets/images/about/about-us-half.png" alt="" />
+            <h1>About Us</h1>
+          </div>
+          <div className="right">
+            <p>
+              Teafusion is a quality tea brand specializing in organic herbal tea.
+              Targeting celebrated tea lovers and health-conscious individuals, our
+              goal is to share a warm, hearty cup of tea with the world.
+            </p>
+            <a href="/about">
+              <button>Learn more</button>
+            </a>
+            <img src="../assets/images/home/usda-organic.png" alt="" />
+            <img src="../assets/images/home/non-gmo.png" alt="" />
+          </div>
+        </div>
+        {/* Join us block */}
+        <div className="ab-block join-us-block">
+          <div className="ab-left">
+            <h1>Join us</h1>
+            <p>
+              Join our email list to get 10% off and be the first to know about new
+              products and special offers.
+            </p>
+            <form>
+              <label htmlFor="email"></label>
+              <input type="email" id="email" name="email" placeholder="Your email address" />
+              <button type="submit">Sign up</button>
+            </form>
+          </div>
+          <div className="join-right">
+            <img src="../assets/images/home/join-us.png" alt="" />
+          </div>
+        </div>
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
+  );
+}
